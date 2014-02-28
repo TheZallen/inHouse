6f81e704fbab526b5f5e2c9f2afe58841c57c6cd
TheZallen <ZacharyAllen2016@u.northwestern.edu>
2014-02-28 13:28:51 -0600

    sorting by name works

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 $( document ).ready(function() {
-var $container = $('#containerDorm');
-// init
-$container.isotope({
-  // options
-  itemSelector: '.item',
-  layoutMode: 'fitRows'
-});
+	var $container = $('#containerDorm');
+	// init
+	$container.isotope({
+	  // options
+	  itemSelector: '.item',
+	  layoutMode: 'fitRows',
+	  getSortData: {
+	    name: '.name',
+	    symbol: '.symbol',
+	    number: '.number parseInt',
+	    category: '[data-category]',
+	    weight: function( itemElem ) {
+	      var weight = $( itemElem ).find('.weight').text();
+	      return parseFloat( weight.replace( /[\(\)]/g, '') );
+	    }
+	  }
+	});
+
+	// sort items on button click
+	$('#sorts').on( 'click', 'button', function() {
+	  var sortValue = $(this).attr('data-sort-value');
+	  $container.isotope({ sortBy: sortValue });
+	});
 });
