16df05765024db608d7aaa547c67c2c2fbb64ba2
TheZallen <ZacharyAllen2016@u.northwestern.edu>
2014-03-01 14:05:30 -0600

    some stuff

--- main.js
+++ main.js
@@ -11,12 +11,19 @@ $( document ).ready(function() {
 	    dining: '.dining',
 	    north: '.north',
 	    south: '.south',
+	    small: '.small',
+	    medium: '.medium',
+	    large: '.large',
+	    single: '.single',
+	    double: '.double',
 	    weight: function( itemElem ) {
 	      var weight = $( itemElem ).find('.weight').text();
 	      return parseFloat( weight.replace( /[\(\)]/g, '') );
 	    }
 	  }
 	});
+
+	$container.isotope({ sortBy : 'name' });
 
 	// sort items on button click
 	$('#sorts').on( 'click', 'button', function() {
