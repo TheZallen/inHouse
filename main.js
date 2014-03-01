02dc69be79ee0812a45dd999ab3eb38cf2b1a187
TheZallen <ZacharyAllen2016@u.northwestern.edu>
2014-03-01 14:53:28 -0600

    popping

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
 	var $container = $('#containerDorm');
-	var dorms [
+	var dorms = [
 	{	
 		"name":"Elder"
 		"ac": true,
@@ -43,8 +43,43 @@ $( document ).ready(function() {
 	$container.isotope({ sortBy : 'name' });
 
 	// sort items on button click
-	$('#sorts').on( 'click', 'button', function() {
+	$('#sorts').on( 'click', 'input', function() {
+
 	  var sortValue = $(this).attr('data-sort-value');
+	  calculateDormRatings(sortValue);
 	  $container.isotope({ sortBy: sortValue });
 	});
+
+	function bindSomeCheckboxes() {
+	    var score=0;
+	    var checkboxes = {
+	        "checked1" : 1,
+	        "checked2" : 2,
+	        "checked3" : 3,
+	        "checked4" : 4,
+	        "checked5" : 5,
+	        "checked6" : 6,
+	        "checked7" : 7,
+	        "checked8" : 8,
+	        "checked9" : 9,
+	        "checked0" : 0
+	    };
+
+	    var arrayOfIds = []; // to store array of ids
+
+	    $.each(checkboxes,function(key, val) {  // populate array with ids
+	        arrayOfIds.push( key );
+	    });
+
+	       // create a selector of the IDs
+	    $( "#" + arrayOfIds.join(',#') ).change(function() {
+	          // alert the score
+	        alert( checkboxes[ this.id ] );
+	        if ( this.checked ) {
+	            //do something when it is checked
+	        } else {
+	            //do something else
+	        }
+	    });
+	} 
 });
