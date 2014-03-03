1f487d1d2bd11104aa59f9e5bf4319e186654ec4
TheZallen <ZacharyAllen2016@u.northwestern.edu>
2014-03-02 19:26:39 -0600

    filtering works but doesn't look good

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
 	var $container = $('#containerDorm');
 	var dorms = [
 	{	
-		"name":"Elder"
+		"name":"elder",
 		"ac": true,
 		"dining": true,
 		"north": true,
@@ -11,12 +11,119 @@ $( document ).ready(function() {
 		"medium": false,
 		"large": true,
 		"single": false,
-		"double": true 
+		"double": true,
+		"rating": 0
 	},
 	{
-
+		"name":"bobb",
+		"ac": true,
+		"dining": false,
+		"north": true,
+		"south": false,
+		"small": false,
+		"medium": false,
+		"large": true,
+		"single": false,
+		"double": true,
+		"rating": 0
+	},
+	{
+		"name":"allison",
+		"ac": true,
+		"dining": true,
+		"north": false,
+		"south": true,
+		"small": false,
+		"medium": false,
+		"large": true,
+		"single": false,
+		"double": true,
+		"rating": 0
+	},
+	{
+		"name":"ayers",
+		"ac": true,
+		"dining": false,
+		"north": true,
+		"south": false,
+		"small": false,
+		"medium": true,
+		"large": false,
+		"single": false,
+		"double": true,
+		"rating": 0
+	}
+	,{
+		"name":"hinman",
+		"ac": true,
+		"dining": true,
+		"north": false,
+		"south": true,
+		"small": false,
+		"medium": false,
+		"large": true,
+		"single": false,
+		"double": true,
+		"rating": 0
+	}
+	,{
+		"name":"plex",
+		"ac": true,
+		"dining": true,
+		"north": false,
+		"south": false,
+		"small": false,
+		"medium": false,
+		"large": true,
+		"single": true,
+		"double": false,
+		"rating": 0
+	},
+	{
+		"name":"sargent",
+		"ac": false,
+		"dining": true,
+		"north": true,
+		"south": false,
+		"small": false,
+		"medium": true,
+		"large": false,
+		"single": false,
+		"double": true,
+		"rating": 0
+	},
+	{
+		"name":"shepard",
+		"ac": true,
+		"dining": false,
+		"north": false,
+		"south": true,
+		"small": false,
+		"medium": true,
+		"large": false,
+		"single": true,
+		"double": true,
+		"rating": 0
+	},
+	{
+		"name":"slivka",
+		"ac": true,
+		"dining": false,
+		"north": true,
+		"south": false,
+		"small": false,
+		"medium": true,
+		"large": false,
+		"single": true,
+		"double": true,
+		"rating": 0
 	}
 	];
+
+	var filtersSelected = 0;
+	var filterArr = [];
+
+	bindSomeCheckboxes();
 	// init
 	$container.isotope({
 	  // options
@@ -24,45 +131,32 @@ $( document ).ready(function() {
 	  layoutMode: 'fitRows',
 	  getSortData: {
 	    name: '.name',
-	    ac: '.ac',
-	    dining: '.dining',
-	    north: '.north',
-	    south: '.south',
-	    small: '.small',
-	    medium: '.medium',
-	    large: '.large',
-	    single: '.single',
-	    double: '.double',
-	    weight: function( itemElem ) {
-	      var weight = $( itemElem ).find('.weight').text();
-	      return parseFloat( weight.replace( /[\(\)]/g, '') );
-	    }
+	    rating: '.rating parseFloat'
 	  }
 	});
 
 	$container.isotope({ sortBy : 'name' });
 
 	// sort items on button click
-	$('#sorts').on( 'click', 'input', function() {
+	/*$('#sorts').on( 'click', 'input', function() {
 
 	  var sortValue = $(this).attr('data-sort-value');
-	  calculateDormRatings(sortValue);
+	  //calculateDormRatings(sortValue);
 	  $container.isotope({ sortBy: sortValue });
-	});
+	});*/
 
 	function bindSomeCheckboxes() {
 	    var score=0;
 	    var checkboxes = {
-	        "checked1" : 1,
-	        "checked2" : 2,
-	        "checked3" : 3,
-	        "checked4" : 4,
-	        "checked5" : 5,
-	        "checked6" : 6,
-	        "checked7" : 7,
-	        "checked8" : 8,
-	        "checked9" : 9,
-	        "checked0" : 0
+	        "check2" : "north",
+	        "check3" : "south",
+	        "check4" : "dining",
+	        "check5" : "ac",
+	        "check6" : "small",
+	        "check7" : "medium",
+	        "check8" : "large",
+	        "check9" : "single",
+	        "check0" : "double"
 	    };
 
 	    var arrayOfIds = []; // to store array of ids
@@ -73,13 +167,50 @@ $( document ).ready(function() {
 
 	       // create a selector of the IDs
 	    $( "#" + arrayOfIds.join(',#') ).change(function() {
-	          // alert the score
-	        alert( checkboxes[ this.id ] );
-	        if ( this.checked ) {
-	            //do something when it is checked
+
+	    	var selected = false;
+	    	if ( this.checked ) {
+	    		filterArr.push(this.id);
+	            filtersSelected++;
+	            selected = true;
 	        } else {
-	            //do something else
+	            filtersSelected--;
+	            var index = filterArr.indexOf(this.id);
+	            if (index > -1) {
+				    filterArr.splice(index, 1);
+				}
+	        }
+	        if (filtersSelected == 0){
+	        	var arrayLength = dorms.length;
+				for (var i = 0; i < arrayLength; i++) {
+					dorms[i].rating = 0;
+					$('#' + dorms[i].name + 'Rat').text("" + dorms[i].rating);
+				}
+	        	$container.isotope({ sortBy : 'name', sortAscending:true });
+	        }else{
+	        	calculateDormRatings(this.id, selected);
+	        	$container.isotope('updateSortData').isotope();
+	        	$container.isotope({ sortBy : 'rating', sortAscending:false });
 	        }
 	    });
+	}
+	function calculateDormRatings(filter, selected){
+		var arrayLength = dorms.length;
+		for (var i = 0; i < arrayLength; i++) {
+			var score = 0;
+			for (var k = 0; k < filterArr.length; k++){
+				if (filterArr[k] == "check5" && dorms[i].ac){score++};
+				if (filterArr[k] == "check4" && dorms[i].dining){score++};
+				if (filterArr[k] == "check2" && dorms[i].north){score++};
+				if (filterArr[k] == "check3" && dorms[i].south){score++};
+				if (filterArr[k] == "check6" && dorms[i].small){score++};
+				if (filterArr[k] == "check7" && dorms[i].medium){score++};
+				if (filterArr[k] == "check8" && dorms[i].large){score++};
+				if (filterArr[k] == "check9" && dorms[i].single){score++};
+				if (filterArr[k] == "check0" && dorms[i].double){score++};
+			}
+		    dorms[i].rating = Math.round((score/filtersSelected)*100);
+		  	$('#' + dorms[i].name + 'Rat').text("" + dorms[i].rating);
+		}
 	} 
 });
